56b3ef37d85058b4d7c6949964205c48d3ac3d1b
LebgaLesly <lebgalesly1@gmail.com>
2023-10-15 22:27:47 -0700

    Portfolio Done

---
+++ src/App.js
@@ -0,0 +1,24 @@
+//COMPONENTS IMPORT
+import About from "./Components/About";
+import Experience from "./Components/Experience";
+import Home from "./Components/Home";
+import NavBar from "./Components/Navbar";
+import Portfolio from "./Components/Portfolio";
+import SocialLinks from "./Components/SocialLinks";
+import Contact from "./Components/Contact";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+      <About />
+      <Portfolio />
+      <Experience />
+      <Contact />
+      <SocialLinks />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,33 @@
+import me from '../Assets/me.jpg'
+import { MdOutlineKeyboardArrowRight } from "react-icons/md"
+import { Link } from "react-scroll"
+ 
+const Home = () => {
+  return (
+    <div name='home' className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 '>
+      <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row '>
+        <div className='flex flex-col justify-center h-full pt-40'>
+            <h2 className='text-4xl sm:text-7xl font-bold text-white '>
+                I'm a Full Stack Developer
+            </h2>
+            <p className='text-gray-500 py-4 max-w-md'>
+                I have 2 years of experience in building and designing web applications. I make use of technologies like React, Redux, expressjs , Nodejs and MongoDb
+            </p>
+            <div>
+                <Link className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-orange-400 to-orange-600 cursor-pointer'>
+                    Portfolio
+                    <span className='group-hover:rotate-90 duration-300'>
+                        <MdOutlineKeyboardArrowRight  size={25} className='ml-1'/> 
+                    </span>
+                </Link>
+            </div>
+        </div>
+        <div>
+            <img src={me} alt="my profile" className='rounded-2xl mx-auto w-2/3  ' />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
