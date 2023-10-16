f5ca262f6f71e927e4a5fb50e8513bf85a088127
LebgaLesly <lebgalesly1@gmail.com>
2023-10-16 02:10:52 -0700

    Structure re-arranged

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 //COMPONENTS IMPORT
-import About from "./Components/About";
-import Experience from "./Components/Experience";
-import Home from "./Components/Home";
-import NavBar from "./Components/Navbar";
-import Portfolio from "./Components/Portfolio";
-import SocialLinks from "./Components/SocialLinks";
-import Contact from "./Components/Contact";
+import About from './Components/About';
+import Experience from './Components/Experience';
+import Home from './Components/Home';
+import NavBar from './Components/Navbar';
+import Portfolio from './Components/Portfolio';
+import SocialLinks from './Components/SocialLinks';
+import Contact from './Components/Contact';
 
 function App() {
   return (
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,37 +1,57 @@
-
-
-
 const About = () => {
   return (
-    <div 
-    name="about"
-    className='w-full  bg-gradient-to-b from-gray-800 to-black text-white'
+    <div
+      name="about"
+      className="w-full  bg-gradient-to-b from-gray-800 to-black text-white"
     >
-        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full pt-40'>
-            <div className="pb-8">
-                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
-                    About
-                </p>
-            </div>
-
-            <p className="text-xl mt-10">
-            I'm a full stack developer with 2 years of experience in web development, I have a strong command over both front-end and back-end technologies, allowing me to build complete and scalable applications.On the front-end side, I specialize in HTML, CSS, and JavaScript frameworks such as React. I'm passionate about creating intuitive user interfaces that provide an exceptional user experience. I pay great attention to detail, ensuring that the websites or applications I develop are responsive, visually appealing, and accessible across different devices and browsers
-            </p>
-
-            <br />
-
-            <p className="text-xl mt-10">
-            Moving to the back end, I'm proficient in server-side languages like Python, Java, or Node.js. I have extensive experience working with databases such as PostgreSQL, and MongoDB, and I can design efficient database structures and write optimized queries. I'm familiar with RESTful APIs and know how to integrate them into applications to enable seamless communication between the front end and back end.Additionally, I have expertise in version control systems like Git, and I'm comfortable working in collaborative environments using Agile methodologies. I enjoy staying up to date with the latest trends and technologies in the web development world, as it allows me to continually improve my skills and deliver cutting-edge solutions.
-            </p>
-
-            <br />
-
-            <p className="text-xl mt-10">
-            I thrive in dynamic environments where I can tackle challenges, collaborate with cross-functional teams, and deliver high-quality solutions that meet both user requirements and business objectives.I'm excited to be part of a team where I can contribute my full stack development skills and help create innovative and impactful applications.
-            </p>
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full pt-40">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
         </div>
-    </div>
-  )
-}
 
-export default About
+        <p className="text-xl mt-10">
+          I'm a full stack developer with 2 years of experience in web
+          development, I have a strong command over both front-end and back-end
+          technologies, allowing me to build complete and scalable
+          applications.On the front-end side, I specialize in HTML, CSS, and
+          JavaScript frameworks such as React. I'm passionate about creating
+          intuitive user interfaces that provide an exceptional user experience.
+          I pay great attention to detail, ensuring that the websites or
+          applications I develop are responsive, visually appealing, and
+          accessible across different devices and browsers
+        </p>
+
+        <br />
+
+        <p className="text-xl mt-10">
+          Moving to the back end, I'm proficient in server-side languages like
+          Python, Java, or Node.js. I have extensive experience working with
+          databases such as PostgreSQL, and MongoDB, and I can design efficient
+          database structures and write optimized queries. I'm familiar with
+          RESTful APIs and know how to integrate them into applications to
+          enable seamless communication between the front end and back
+          end.Additionally, I have expertise in version control systems like
+          Git, and I'm comfortable working in collaborative environments using
+          Agile methodologies. I enjoy staying up to date with the latest trends
+          and technologies in the web development world, as it allows me to
+          continually improve my skills and deliver cutting-edge solutions.
+        </p>
+
+        <br />
+
+        <p className="text-xl mt-10">
+          I thrive in dynamic environments where I can tackle challenges,
+          collaborate with cross-functional teams, and deliver high-quality
+          solutions that meet both user requirements and business objectives.I'm
+          excited to be part of a team where I can contribute my full stack
+          development skills and help create innovative and impactful
+          applications.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
